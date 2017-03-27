9f696a40e94f0ba280cf51b820ca709e60148993
shuttlesworthNEO <gupta.puneet0696@gmail.com>
2017-03-28 00:45:37 +0530

    Model Queries updates

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Post
+from django.utils import timezone
+
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.all().order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
